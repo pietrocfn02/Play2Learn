b92aaeb0d2df8a56317c580aaeae809f1785e229
MartinaCanonPh <marti2f.mc@gmail.com>
2022-05-20 19:06:53 +0200

    added new objects
    new objects in the rooms

---
+++ Prova Progetto/Assets/Scripts/SpawnOneTypeCoin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOneTypeCoin : MonoBehaviour
+{
+    // Coin Prefab
+    [SerializeField] private GameObject coinPrefab;
+    private GameObject[] _coins;
+    public int coinCount;
+    
+    void Start()
+    {
+        _coins = new GameObject[coinCount];
+    }
+
+    // Update is called once per frame
+    async void Update()
+    {
+        for(int i=0; i<_coins.Length; i++)
+        {
+            if(_coins[i] == null)
+            {
+                _coins[i] = Instantiate(coinPrefab) as GameObject;
+                _coins[i].transform.position = new Vector3(Random.Range(-20f,20f), Random.Range(-1.4f, -0.2f), Random.Range(-20f,20f));
+                _coins[i].transform.localScale += new Vector3(3f,3f,1f);
+                float angle = Random.Range (0, 360f);
+                _coins[i].transform.Rotate(0, angle, 0);
+            }
+        }        
+    }
+}
