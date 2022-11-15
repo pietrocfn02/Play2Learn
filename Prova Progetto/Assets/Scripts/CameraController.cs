18a7ee5378282dbd401ed3419b88dca5191e80b0
pietrocf02@outlook.it <pietrocf02@outlook.it>
2022-11-15 16:16:45 +0100

    camere come se piovesse

--- Prova Progetto/Assets/Scripts/CameraController.cs
+++ Prova Progetto/Assets/Scripts/CameraController.cs
@@ -7,7 +7,7 @@ public class CameraController : MonoBehaviour
 
 
      private List<CameraScript> cameras = new List<CameraScript>();
-     private string[] tags = {"Camera_Corridoi","Camera_Ingresso"};
+     private string[] tags = {"Camera_Corridoi","Camera_Ingresso", "Camera_Cucina", "Camera_Altarino", "Camera_Bedroom1", "Camera_Bedroom2"};
 
     void Start() {
        
@@ -24,13 +24,14 @@ public class CameraController : MonoBehaviour
          foreach (string tag in tags) {
             GameObject[] camerasTmp =  GameObject.FindGameObjectsWithTag(tag+"_CAM");
             if (camerasTmp.Length > 0) {
+                Debug.Log("Add for "+tag);
                 cameras.Add(camerasTmp[0].GetComponent<CameraScript>());
             }
             
         }
         for (int i=0 ; i < tags.Length; i++) {
                 Debug.Log("Entro qui!");
-                Debug.Log(cameras.ToArray().Length);
+                Debug.Log(cameras[i].tag+" "+i);
                 Messenger.AddListener("ACTIVATE_CAMERA_"+tags[i], cameras[i].activateMe);
                 Messenger.AddListener("DEACTIVATE_CAMERA_"+tags[i], cameras[i].deactivateMe);
         } 
