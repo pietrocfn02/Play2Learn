c6754871a50682ca34199aa1dd423c27e9b65cd0
MartinaCanonPh <marti2f.mc@gmail.com>
2022-11-16 12:28:27 +0100

    Floater script added
    now Dory can float!

--- Prova Progetto/Assets/Scripts/CameraController.cs
+++ Prova Progetto/Assets/Scripts/CameraController.cs
@@ -6,37 +6,37 @@ public class CameraController : MonoBehaviour
 {
 
 
-     private List<CameraScript> cameras = new List<CameraScript>();
-     private string[] tags = {"Camera_Corridoi","Camera_Ingresso", "Camera_Cucina", "Camera_Altarino", "Camera_Bedroom1", "Camera_Bedroom2"};
+    //  private List<CameraScript> cameras = new List<CameraScript>();
+    //  private string[] tags = {"Camera_Corridoi","Camera_Ingresso", "Camera_Cucina", "Camera_Altarino", "Camera_Bedroom1", "Camera_Bedroom2"};
 
-    void Start() {
+    // void Start() {
        
 
-    }
+    // }
 
-    void Update() {
+    // void Update() {
 
-    }   
+    // }   
 
-    void Awake() {
+    // void Awake() {
 
-        //Suppongo la corrispondenza tra i tag e le camere
-         foreach (string tag in tags) {
-            GameObject[] camerasTmp =  GameObject.FindGameObjectsWithTag(tag+"_CAM");
-            if (camerasTmp.Length > 0) {
-                Debug.Log("Add for "+tag);
-                cameras.Add(camerasTmp[0].GetComponent<CameraScript>());
-            }
+    //     //Suppongo la corrispondenza tra i tag e le camere
+    //      foreach (string tag in tags) {
+    //         GameObject[] camerasTmp =  GameObject.FindGameObjectsWithTag(tag+"_CAM");
+    //         if (camerasTmp.Length > 0) {
+    //             Debug.Log("Add for "+tag);
+    //             cameras.Add(camerasTmp[0].GetComponent<CameraScript>());
+    //         }
             
-        }
-        for (int i=0 ; i < tags.Length; i++) {
-                Debug.Log("Entro qui!");
-                Debug.Log(cameras[i].tag+" "+i);
-                Messenger.AddListener("ACTIVATE_CAMERA_"+tags[i], cameras[i].activateMe);
-                Messenger.AddListener("DEACTIVATE_CAMERA_"+tags[i], cameras[i].deactivateMe);
-        } 
+    //     }
+    //     for (int i=0 ; i < tags.Length; i++) {
+    //             Debug.Log("Entro qui!");
+    //             Debug.Log(cameras[i].tag+" "+i);
+    //             Messenger.AddListener("ACTIVATE_CAMERA_"+tags[i], cameras[i].activateMe);
+    //             Messenger.AddListener("DEACTIVATE_CAMERA_"+tags[i], cameras[i].deactivateMe);
+    //     } 
         
-    }
+    // }
 
 
     
