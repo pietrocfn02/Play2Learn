19e4613bc3261c4cd96b7e97445e998336ad75dc
Eltio00 <elioc00@outlook.it>
2022-12-01 18:00:49 +0100

    Seconda missione finita

--- Prova Progetto/Assets/Scripts/UIAngioletto.cs
+++ Prova Progetto/Assets/Scripts/UIAngioletto.cs
@@ -11,42 +11,85 @@ public class UIAngioletto : MonoBehaviour
     
     
     [SerializeField] private TMP_Text countText; // testo inventario
-    [SerializeField] private TMP_Text labelText;
-    [SerializeField] private GameObject imageText;
+    [SerializeField] private TMP_Text labelText; // testo movimento
+    [SerializeField] private GameObject imageText;  // immagine testo
     [SerializeField] private TMP_Text labelMission; // testo missione
     [SerializeField] private Image inventaryImage; // immagine inventario
-    [SerializeField] private Sprite remoteImage; // immagine telecomando
+    [SerializeField] private Sprite tvImage; // immagine telecomando
+    [SerializeField] private Sprite bookImage; // immagine libri
+    [SerializeField] private TMP_Text quantityText; // testo quantità
+
     
     
 
     int angioletto_score;
     int count = 0;
 
-    void Start()
-    {
+    void Start(){
     }
 
+    IEnumerator pastelliMission(){
+        labelText.text = "";
+        imageText.SetActive(true);
+        countText.text = "0";
+        labelText.text = "COMPLIMENTI HAI FINITO DI RIORDINARE LA STANZA!";
+        yield return new WaitForSecondsRealtime(2);
+        labelText.text = "MAH?!... NON SENTI ANCHE TU DEI RUMORI?";
+        yield return new WaitForSecondsRealtime(2);
+        labelText.text = "DEVI AVER LASCIATO LE TV ACCESE!";
+        yield return new WaitForSecondsRealtime(2);
+        labelText.text = "FORZA! CERCA UN TELECOMANDO E SPEGNILE TUTTE!";
+        quantityText.text = "1";
+        inventaryImage.sprite = tvImage;
+        labelMission.text = "Raccogli un telecomando e spegni le TV.";
+        yield return new WaitForSecondsRealtime(2);
+        imageText.SetActive(false);
+        // aprire la porta quando finisce la missione
+        count = 0;
+    }
+
+    IEnumerator tvMission(){
+        labelText.text = "";
+        imageText.SetActive(true);
+        countText.text = "0";
+        labelText.text = "MA CHE BRAVA, HAI SPENTO TUTTO!";
+        yield return new WaitForSecondsRealtime(2);
+        labelText.text = "ADESSO MANCA SOLTANTO UNA COSA DA FARE...";
+        yield return new WaitForSecondsRealtime(2);
+        labelText.text = "E BENE SI... ORA E' ARRIVATO IL MOMENTO DI FARE I COMPITI!!!";
+        yield return new WaitForSecondsRealtime(2);
+        labelText.text = "ANDIAMO! CERCA IN CASA UN QUADERNO E POI VAI SULLA SCRIVANIA!";
+        quantityText.text = "1";
+        inventaryImage.sprite = bookImage;
+        labelMission.text = "Prendi un quaderno e fai i compiti";
+        yield return new WaitForSecondsRealtime(2);
+        imageText.SetActive(false);
+        // aprire la porta quando finisce la missione
+        count = 0;
+    }
 
     void Awake() {
         Messenger.AddListener(GameEvent.ANGIOLETTO_UPDATE, updateAngiolettoScore);
         Messenger.AddListener(GameEvent.RACCOLTA_UPDATE, updateInventary);
         Messenger.AddListener(GameEvent.LANCIA_OGGETTO, updateInventary);
         Messenger.AddListener(GameEvent.MISSIONE_PASTELLI, completePastelli);
+        Messenger.AddListener(GameEvent.MISSIONE_TELEVISIONI, completeTelevisioni);
     }
     
+    public void completeTelevisioni(){
+        StartCoroutine(tvMission());
+    }
+
     private void updateInventary(){
         count++;
         countText.text = count.ToString();
         if(count == 12){
+            imageText.SetActive(true);
             labelText.text="Hai raccolto tutti i pastelli! Ora rimettili a posto nel cestino in mezzo alla stanza!";
         }
     }
-    
     private void completePastelli(){
-        countText.text = "0";
-        labelText.text = "BRAVO HAI FINITO DI RIORDINARE LA STANZA, ORA PASSA ALLA PROSSIMA BUONA AZIONE!";
-        inventaryImage.sprite = remoteImage;
-        labelMission.text = "Raccogli un telecomando e spegni le TV.";
+        StartCoroutine(pastelliMission());
     }
     
     private void updateAngiolettoScore() {
@@ -55,19 +98,13 @@ public class UIAngioletto : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if(labelText.text =="")
-        {
-            imageText.SetActive(false);
-        } else {
-            imageText.SetActive(true);
-        }
-    }
+    void Update(){}
+
     void OnDestroy() {
         Messenger.RemoveListener(GameEvent.ANGIOLETTO_UPDATE, updateAngiolettoScore );
         Messenger.RemoveListener(GameEvent.RACCOLTA_UPDATE, updateInventary );
         Messenger.RemoveListener(GameEvent.LANCIA_OGGETTO, updateInventary );
         Messenger.RemoveListener(GameEvent.MISSIONE_PASTELLI, completePastelli);
+        Messenger.RemoveListener(GameEvent.MISSIONE_TELEVISIONI, completeTelevisioni);
     }
 }
