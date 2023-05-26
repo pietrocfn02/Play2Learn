9d3a5aa247885a5e9d7b2bbe05bd897bf55e3e7d
Elio <elioc00@outlook.it>
2023-05-26 12:01:11 +0200

    Push goliardico

--- Prova Progetto/Assets/Scripts/CalculatorEvent.cs
+++ Prova Progetto/Assets/Scripts/CalculatorEvent.cs
@@ -9,31 +9,32 @@ public class CalculatorEvent : MonoBehaviour
 {
     //[SerializeField] private GameObject calculator;
     [SerializeField] private TMP_Text valueText;
-    [SerializeField] private Camera camera;
-    private LayerMask mask;
+    private Camera camera;
+    public LayerMask mask;
     private GameObject calculator;
+    
     void Start()
     {
-        camera = GameObject.Find("Camera").GetComponent<Camera>();
-        
+        camera = Camera.main;
+        Debug.Log(camera.name);
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition.z = 100f;
         mousePosition = camera.ScreenToWorldPoint(mousePosition);
         Debug.DrawRay(transform.position, mousePosition - transform.position, Color.blue);
-
+        
         if (Input.GetMouseButtonUp(0))
         {
-            //Debug.Log(mousePosition);
-            Ray ray = camera.ScreenPointToRay(mousePosition);
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+
             if (Physics.Raycast(ray,out hit, 100, mask))
             {
                 Debug.Log(hit.transform.name);
+                Debug.Log("hit something");
             }
         }
     }
